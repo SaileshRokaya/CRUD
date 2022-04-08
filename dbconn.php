72bdcd599a43740c550d2ae5fed75192cce62182
SaileshRokaya <79441756+SaileshRokaya@users.noreply.github.com>
2022-04-08 11:47:08 +0545

    first commit

---
+++ dbconn.php
@@ -0,0 +1,10 @@
+<?php
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $con = mysqli_connect($dbhost,$dbuser,$dbpass, "testpcps");
+
+    if (!$con){
+        die('Connect Error: ' .mysqli_connect_errno());
+    }
+?>
