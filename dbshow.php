fe5cab7b291cc3bb8578b5954baeccfb22947bcb
SaileshRokaya <79441756+SaileshRokaya@users.noreply.github.com>
2022-04-08 12:33:04 +0545

    show data in tables

--- dbshow.php
+++ dbshow.php
@@ -4,12 +4,34 @@
     $cnt=0;
     $sql="select * from student";
     $res=mysqli_query($con, "select * from student");
-
+/*
         while($row=mysqli_fetch_assoc($res))
         {
             echo $row['name']."<br>";
             $cnt=$cnt+1;
         }
     echo "<br>Total Number of Data Displayed: ".$cnt."<br>";
-
+*/
 ?>
+
+
+<table border="1">
+    <tr>
+        <th>Name</th>
+        <th>Contact</th>
+    </tr>
+
+    <?php
+        if(mysqli_num_rows($res)>0){
+            while($row=mysqli_fetch_assoc($res))
+            {
+                echo "<tr>";
+                echo "<td>".$row['name']."</td><td>".$row['contact']."</td>";
+                echo "<tr>";
+                $cnt=$cnt+1;
+            }
+        }
+        echo "<br> Total Number of Data Displayed: ".$cnt."<br>";
+    ?>
+    
+</table>
